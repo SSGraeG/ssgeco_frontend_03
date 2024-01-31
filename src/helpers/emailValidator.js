81f890c3a39ff739d56fadf06b171384fb75ee38
hwan515 <stophwan515@gmail.com>
2024-01-31 18:47:53 +0900

    초기 커밋 메시지

--- src/helpers/emailValidator.js
+++
@@ -1,6 +0,0 @@
-export function emailValidator(email) {
-  const re = /\S+@\S+\.\S+/
-  if (!email) return "이메일을 입력해주세요"
-  if (!re.test(email)) return '유효한 이메일을 입력해주세요'
-  return ''
-}
--- src/helpers/nameValidator.js
+++
@@ -1,4 +0,0 @@
-export function nameValidator(name) {
-  if (!name) return "사용자 이름을 입력해주세요"
-  return ''
-}
